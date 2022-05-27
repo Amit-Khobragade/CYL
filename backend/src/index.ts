1b2f073d644a07af737a35ca20cd77fdcdc4c4a2
Amit Khobragade <amitkh1102@outlook.com>
2022-05-27 19:16:20 +0530

    deleted old and unused code from src the get and post files were unecessary for the graphql version of the application also removed the redundant code from index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,1 @@
-import postRouter from "./post";
 import Express from "express";
-import { initializeApp } from "firebase-admin/app";
-import { credential } from "firebase-admin";
-
-initializeApp({
-  credential: credential.cert("src/auth/auth-key.json"),
-});
-
-const app = Express();
-
-app.use(Express.json());
-app.use(postRouter);
-
-app.listen(3000);
