3dfa858efec0823bcae86dd27b50e5c86f019d64
Amit Khobragade <amitkh1102@outlook.com>
2022-05-27 19:08:28 +0530

    Initial steps to add gql

--- backend/src/gqlschemas/users.ts
+++ backend/src/gqlschemas/users.ts
@@ -1,1 +1,30 @@
 import * as data from "../Data/Data";
+import { GraphQLObjectType, GraphQLString, GraphQLBoolean } from "graphql";
+import { UserData } from "../interfaces";
+
+const UserType = new GraphQLObjectType({
+  name: "users",
+  fields: () => ({
+    uid: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phoneNumber: { type: GraphQLString },
+    displayName: { type: GraphQLString },
+    isStore: { type: GraphQLBoolean },
+    isTrusted: { type: GraphQLBoolean },
+    jwt: { type: GraphQLString },
+  }),
+});
+
+const RootUser = new GraphQLObjectType({
+  name: "RootUserType",
+  fields: {
+    user: {
+      type: UserType,
+      args: {
+        uid: { type: GraphQLString },
+      },
+      resolve: (parent, args): UserData | undefined =>
+        data.getUserById(args.uid),
+    },
+  },
+});
