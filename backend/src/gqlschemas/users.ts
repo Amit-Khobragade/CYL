917796a068f3fa0368e90304a13e90eb9b0d6d13
Amit Khobragade <amitkh1102@outlook.com>
2022-05-28 19:26:23 +0530

    Added new end points for gql these end points include check jwt ,add user ,all user infos, login, etc.

--- backend/src/gqlschemas/users.ts
+++ backend/src/gqlschemas/users.ts
@@ -1,11 +1,12 @@
-import * as data from "../Data/Data";
+import * as data from "../Data/userData";
 import {
   GraphQLObjectType,
   GraphQLString,
   GraphQLBoolean,
   GraphQLSchema,
+  GraphQLList,
 } from "graphql";
-import { UserData } from "../interfaces";
+import { isNewUserData, isUserData, UserData } from "../interfaces";
 
 const UserType = new GraphQLObjectType({
   name: "users",
@@ -31,6 +32,64 @@ const schema: GraphQLSchema = new GraphQLSchema({
         },
         resolve: (parent, args): UserData | undefined =>
           data.getUserById(args.uid),
+      },
+      checkJwt: {
+        type: GraphQLBoolean,
+        args: {
+          uid: { type: GraphQLString },
+          jwt: { type: GraphQLString },
+        },
+        resolve: (parent, args): Boolean => data.checkJwt(args.uid, args.jwt),
+      },
+      //! DEVELOPER END POINT MUST BE REMOVED BEFORE PRODUCTION
+      users: {
+        type: new GraphQLList(UserType),
+        resolve: (parent, args): UserData[] => data.getAllUsers(),
+      },
+    },
+  }),
+
+  mutation: new GraphQLObjectType({
+    name: "RootMutationType",
+    fields: {
+      userLogin: {
+        type: UserType,
+        args: {
+          email: { type: GraphQLString },
+          password: { type: GraphQLString },
+        },
+        resolve: (parent, args): UserData | undefined =>
+          data.login(args.email, args.password),
+      },
+      createUser: {
+        type: UserType,
+        args: {
+          email: { type: GraphQLString },
+          phoneNumber: { type: GraphQLString },
+          displayName: { type: GraphQLString },
+          password: { type: GraphQLString },
+        },
+        resolve: (parent, args): UserData | null => {
+          return data.addUser(args);
+        },
+      },
+      makeStore: {
+        type: UserType,
+        args: {
+          uid: { type: GraphQLString },
+          jwt: { type: GraphQLString },
+        },
+        resolve: (parent, args): UserData | undefined =>
+          data.makeUserStore(args.uid, args.jwt),
+      },
+      trustStore: {
+        type: UserType,
+        args: {
+          uid: { type: GraphQLString },
+          jwt: { type: GraphQLString },
+        },
+        resolve: (parent, args): UserData | undefined =>
+          data.trustUser(args.uid, args.jwt),
       },
     },
   }),
