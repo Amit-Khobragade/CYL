a28509816c67ce3ad1066d0faf4480c42cdd8838
Amit Khobragade <amitkh1102@outlook.com>
2022-06-20 11:54:03 +0530

    Camera Updates

--- frontend/lib/App/Camera.dart
+++ frontend/lib/App/Camera.dart
@@ -1,3 +1,4 @@
+import 'package:CYL/App/Variables/ColorVariables.dart';
 import 'package:flutter/material.dart';
 import 'package:CYL/Component/CameraComponents/CameraBtn.dart';
 import 'package:CYL/Component/CameraComponents/ImagesBtn.dart';
@@ -20,18 +21,31 @@ class _CameraPageState extends State<CameraPage> {
         child: Stack(
           children: [
             Positioned(
-                bottom: SizeVariables.bottomControlsPosB,
-                left: SizeVariables.bottomControlsPosL,
-                child: SizedBox(
-                  width: (MediaQuery.of(context).size.width + SizeVariables.iconLarge) / 2,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const ImagesBtn(),
-                      ClickBtn(onPressed: () => print('click')),
-                    ],
-                  ),
-                ))
+              bottom: SizeVariables.bottomControlsPosB,
+              left: SizeVariables.bottomControlsPosL,
+              child: SizedBox(
+                width: (MediaQuery.of(context).size.width + SizeVariables.iconLarge) / 2,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const ImagesBtn(),
+                    ClickBtn(onPressed: () => print('click')),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: SizeVariables.bottomControlsPosL,
+              left: SizeVariables.bottomControlsPosL + 10,
+              child: IconButton(
+                icon: const Icon(
+                  Icons.chevron_left_sharp,
+                  color: ColorVariables.color60Per,
+                  size: SizeVariables.iconSmall1,
+                ),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ),
           ],
         ),
       ),
