d2805da5393f091d51b9c562db2075aa3181c1ff
Amit Khobragade <amitkh1102@outlook.com>
2022-06-24 13:15:59 +0530

    Resolved Camera connectivity issues

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import './App/CYL.dart';
 
-void main() {
-  runApp(CYL());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  CameraDescription cameraDescription = (await availableCameras())[1];
+  runApp(CYL(
+    cameraDescription: cameraDescription,
+  ));
 }
